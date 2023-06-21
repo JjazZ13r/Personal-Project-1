d8c9d5f31ef218b64e0153ac073293db0f23171c
Jonthan McAvinue <mcavinuejon@gmail.com>
2023-06-21 16:44:44 -0400

    adding some small ammount of work based on getting properties on manga volume info

--- MangaCollectionLibrary/MangaCollectionLibrary/MangaVolumeInfo.cs
+++ MangaCollectionLibrary/MangaCollectionLibrary/MangaVolumeInfo.cs
@@ -11,13 +11,39 @@ namespace MangaCollectionLibrary
         public string MangaName { get; set; }
         public string MangaMagazine { get; set; }
         public string Genre { get; set; }
-
         public void GetDemographic()
         {
             int userInput = int.Parse(Console.ReadLine());
             mangaDemographic = (Demographic)userInput;
 
 
+        }
+        public void GetMangaNameByUserInput()
+        {
+            Console.WriteLine("What is the title of the Manga Volume?: ");
+            string userInput = Console.ReadLine();
+            this.MangaName = userInput;
+            GetMangaMagazineByUserInput();
+        }
+        public void GetMangaMagazineByUserInput()
+        {
+            Console.WriteLine("What magazine was this Manga published in?: ");
+            string userInput = Console.ReadLine();
+            this.MangaMagazine = userInput;
+            GetGenreByUserInput
+        }
+        public void GetGenreByUserInput()
+        {
+            Console.WriteLine("What Genre is this Manga?: ");
+            string userInput = Console.ReadLine();
+            this.Genre = userInput;
+            Console.WriteLine("Does this have another Genre? Y/N: ");
+            string yesOrNoInput = Console.ReadLine().ToUpper();
+
+            if (yesOrNoInput == "Y")
+            {
+                GetGenreByUserInput();
+            }
         }
     }
 }
