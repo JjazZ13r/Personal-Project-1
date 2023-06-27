68e77d5a6313ccaea61b3f00ffefd61ae0414973
JjazZ13r <mcavinuejon@gmail.com>
2023-06-26 20:18:05 -0400

    adding additional small work

--- MangaCollectionLibrary/MangaCollectionLibrary/Reader.cs
+++ MangaCollectionLibrary/MangaCollectionLibrary/Reader.cs
@@ -19,6 +19,8 @@ namespace MangaCollectionLibrary
                     while (!reader.EndOfStream)
                     {
                         string line = reader.ReadLine();
+                        string[] words = line.Split("|");
+
                         Console.WriteLine(line);
                     }
                 }
